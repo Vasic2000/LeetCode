c78eb4873fa9924236659237b705a7dcf1c1e66a
vasic2000 <vasic2000@yandex.ru>
2023-10-05 20:54:49 +0300

    Done. HashSet

--- src/Easy/CntainsDuplicate.java
+++ src/Easy/CntainsDuplicate.java
@@ -1,5 +1,7 @@
 package Easy;
 
+import java.util.HashSet;
+
 public class CntainsDuplicate {
     public static void main(String[] args) {
 
@@ -12,13 +14,24 @@ public class CntainsDuplicate {
         System.out.println(containsDuplicate(nums3));
     }
 
+//    public static boolean containsDuplicate(int[] nums) {
+//        for(int i = 0; i < nums.length - 1; i++)
+//            for(int j = i + 1; j < nums.length; j++) {
+//                if(nums[i] == nums [j]) {
+//                    return true;
+//                }
+//            }
+//        return false;
+//    }
+
     public static boolean containsDuplicate(int[] nums) {
-        for(int i = 0; i < nums.length - 1; i++)
-            for(int j = i + 1; j < nums.length; j++) {
-                if(nums[i] == nums [j]) {
-                    return true;
-                }
+        HashSet<Integer> store = new HashSet<>();
+
+        for( int i = 0 ; i < nums.length; i++ ) {
+            if(!store.add(nums[i])) {
+                return true;
             }
+        }
         return false;
     }
 
